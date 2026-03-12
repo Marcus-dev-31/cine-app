ca1b1c467a3b9ec656d486bf130de8fca00138fe
Marcos Veliz <marcus.devprojects@gmail.com>
2026-03-12 11:06:03 -0300

    feat: moviedetail con useParams y fetch a película específica

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const MovieDetail = () => {
+
+  const [date, setDate] = useState(null);
+  const [charge, setCharge] = useState(true);
+  const [error, setError] = useState(null);
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=es-ES&page=1`,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setDate(res);
+        setCharge(false);
+      })
+      .catch((error) => {
+        console.error("Error Completo", error);
+        setError("No se pudieron cargar las peliculas");
+        setCharge(false);
+      });
+  }, []);
+
+  if (charge) return <p>Cargando...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div>MovieDetail</div>
-  )
-}
+    <>
+      <h2>MovieDetail</h2>
+      <h3>{date.title}</h3>
+      <p>{date.overview}</p>
+    </>
+  );
+};
